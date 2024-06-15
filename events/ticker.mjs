9cc997542b86f63198f10321e007ef4a38747ae2
freen <freennnn@gmail.com>
2024-06-15 07:55:21 +0200

    - modify algorithm to emit first tick immediately - make sure events and final callback are executed asynchronously

--- events/ticker.mjs
+++ events/ticker.mjs
@@ -1,24 +1,27 @@
 // Example of combining callback and Observer pattern: Events/EventEmitter API style for async task
 import { EventEmitter } from 'events';
+import { nextTick } from 'process'; 
 function ticker(number, callback) {
   const eventEmitter = new EventEmitter();
   let timeElapsed = 0;
   let ticksEmitted = 0;
 
   function tick() {
-    timeElapsed += 50;
     if (timeElapsed < number) {
-      // could be wrapped into process.nextTick() if we didn't control all the call sites - to make sure events
-      // always emitted asynchronously:
-      eventEmitter.emit('tick', ++ticksEmitted); 
+      // emitting event asynchronously to avoid Zalgo
+      nextTick(() => { 
+        eventEmitter.emit('tick', ++ticksEmitted, timeElapsed);
+        timeElapsed += 50;
+      });
       setTimeout(tick, 50);
     } else {
-      callback(null, ticksEmitted); // Node.js error first callback signature pattern
+      nextTick(() => callback(null, ticksEmitted)); // Node.js error first callback signature pattern
     }
+
   }
-  setTimeout(tick, 50);
+  tick();
   return eventEmitter;
 }
 
-ticker(301, (err, count) => console.log(`ticks been emitted ${count}`))
-  .on('tick', (index) => console.log(`ticked ${index}`)) 
+ticker(0, (err, count) => console.log(`ticks been emitted ${count}`))
+  .on('tick', (index, timeElapsed) => console.log(`ticked ${index} at ${timeElapsed}`)) 
