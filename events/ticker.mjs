30fe3a8434bb19964ff734109ae3ba0e618330fa
freen <freennnn@gmail.com>
2024-06-16 08:00:07 +0200

    feat: add error case, add callback error handle logic, add on('error') listener

--- events/ticker.mjs
+++ events/ticker.mjs
@@ -7,6 +7,17 @@ function ticker(number, callback) {
   let ticksEmitted = 0;
 
   function tick() {
+    let currentTime = Date.now();
+    console.log(`currentTime = ${currentTime}`);
+    if (currentTime % 5 === 0) {
+      nextTick(() => {
+        const err = new Error('the timestamp at the moment of a tick is divisible by 5');
+        eventEmitter.emit('error', err);
+        callback(err, null);
+      });
+      return;
+    }
+
     if (timeElapsed < number) {
       // emitting event asynchronously to avoid Zalgo
       nextTick(() => { 
@@ -23,5 +34,12 @@ function ticker(number, callback) {
   return eventEmitter;
 }
 
-ticker(0, (err, count) => console.log(`ticks been emitted ${count}`))
-  .on('tick', (index, timeElapsed) => console.log(`ticked ${index} at ${timeElapsed}`)) 
+ticker(300, (err, count) => {
+    if (err) {
+      console.error(`Callback with error ${err.message}`);
+    } else {
+      console.log(`ticks been emitted ${count}`);
+    }
+  })
+  .on('tick', (index, timeElapsed) => console.log(`ticked ${index} at ${timeElapsed}`))
+  .on('error', (err) => console.error(`Emitted and listened to: ${err}`));
